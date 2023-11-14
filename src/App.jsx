055bdda48a6c551390e1943469937fbf5fa173e6
natalete <natsalete14@gmail.com>
2023-11-14 11:35:34 -0300

    Condicional  React

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import MeuComponente from "./components/MeuComponents";
 
+import MeuContador from "./MeuContador";
+
 function App() {
   return (
     <div>
       <h1>Olá Mundo, React!</h1>
-        <MeuComponente/>
-        {/* <CardDePrograma image='' titulo= 'credit suiss' subtitle='pytho'/> */}
+      <MeuComponente />
+      {/* <CardDePrograma image='' titulo= 'credit suiss' subtitle='pytho'/> */}
 
-        <MeuBotao conteudo= 'me clique'/>
+      <MeuBotao conteudo="me clique" />
 
-        <MeuBotao conteudo= 'depois aqui'/>
+      <MeuBotao conteudo="depois aqui" />
 
-        <MeuBotao conteudo= 'e por fim aqui' idade = {2}/>
+      <MeuBotao conteudo="e por fim aqui" idade={2} />
+
+      <div>
+        <h1>Estados e eventos</h1>
+
+        <MeuContador />
+      </div>
+
     </div>
   );
 }
 
 function MeuBotao(props) {
-
-  return (
-    <button>{props.conteudo}</button>
-  )
+  return <button>{props.conteudo}</button>;
 }
-
 
 export default App;
