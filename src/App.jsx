0c5d5cf9277b04de5246f777aa44860dc6cc055d
natalete <natsalete14@gmail.com>
2023-11-14 13:38:25 -0300

    Efeitos colaterais React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+// import { useState } from "react";
 import MeuComponente from "./components/MeuComponents";
 
 import MeuContador from "./MeuContador";
 
+const minhaLista = [
+  { id: '1', value: 'Fruta' },
+  { id: '2', value: 'Legume' },
+  { id: '3', value: 'Carne' },
+]
+
 function App() {
+
+  const [produtos, setProdutos] = useState(minhaLista);
+  const [pesquisa, setPesquisa] = useState('');
+
+  useEffect(
+    () => {
+    if (pesquisa) {
+      const novaLista = minhaLista.filter( (item) => {
+        return item.value.toLowerCase().includes(pesquisa.toLowerCase())
+      } )
+      setProdutos(novaLista)
+    } else {
+      setProdutos(minhaLista)
+    }
+  }
+  , [pesquisa])
+
   return (
     <div>
       <h1>Olá Mundo, React!</h1>
@@ -21,6 +46,21 @@ function App() {
         <MeuContador />
       </div>
 
+      <div>
+        <h1>Efeitos colaterais</h1>
+        <input
+          value={pesquisa}
+          onChange={(e) => setPesquisa(e.target.value)}
+          placeholder="pesquise aqui"
+        ></input>
+        {produtos.map((item) => {
+          return (
+            <div key={item.id}>
+              <h4>{item.value}</h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
