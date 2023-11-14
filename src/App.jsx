4622514655ddc6e148b7380563554401da962451
natalete <natsalete14@gmail.com>
2023-11-14 14:13:13 -0300

    Buscando dados React

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,42 @@ const minhaLista = [
   { id: '1', value: 'Fruta' },
   { id: '2', value: 'Legume' },
   { id: '3', value: 'Carne' },
-]
+];
+
+const tarefas = [
+  { id: '1', title: 'minha primeira tarefa' },
+  { id: '2', title: 'minha segunda tarefa' },
+  { id: '3', title: 'minha terceira tarefa' },
+  { id: '4', title: 'minha quarta tarefa' },
+];
 
 function App() {
-
   const [produtos, setProdutos] = useState(minhaLista);
+
   const [pesquisa, setPesquisa] = useState('');
 
-  useEffect(
-    () => {
+  const [tarefas, setTarefas] = useState([])
+
+  useEffect(() => {
     if (pesquisa) {
-      const novaLista = minhaLista.filter( (item) => {
-        return item.value.toLowerCase().includes(pesquisa.toLowerCase())
-      } )
-      setProdutos(novaLista)
+      const novaLista = minhaLista.filter((item) => {
+        return item.value.toLowerCase().includes(pesquisa.toLowerCase());
+      });
+      setProdutos(novaLista);
     } else {
-      setProdutos(minhaLista)
+      setProdutos(minhaLista);
     }
-  }
-  , [pesquisa])
+  }, [pesquisa]);
+
+  useEffect(() => {
+    async function buscarDados(){
+        const resultado = await fetch ('https://jsonplaceholder.typicode.com/todos')
+        const resultadoFinal = await resultado.json()
+        return resultadoFinal
+    }
+
+    buscarDados().then(res => setTarefas(res))
+  }, [])
 
   return (
     <div>
@@ -60,6 +77,21 @@ function App() {
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <h1>Buscando dados</h1>
+        <ol>
+          {tarefas.map((tarefas) => {
+            return(
+              <div>
+                <li key={tarefas.id}>{tarefas.title}
+                {tarefas.completed ? '- Tarefa Concluída' : null}</li>;
+                
+               </div>
+            )
+          })}
+        </ol>
       </div>
     </div>
   );
